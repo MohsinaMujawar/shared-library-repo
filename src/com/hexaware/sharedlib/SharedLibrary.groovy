490a883d0011ddd3433c40849d02247d118e23d8
MohsinaMujawar <88731572+MohsinaMujawar@users.noreply.github.com>
2021-12-23 14:51:37 +0530

    Update SharedLibrary.groovy

--- src/com/hexaware/sharedlib/SharedLibrary.groovy
+++ src/com/hexaware/sharedlib/SharedLibrary.groovy
@@ -1,1 +1,14 @@
+package com.hexaware.sharedlib;
 
+public class SharedLibrary{
+  
+  def steps
+  
+  public SharedLibrary(steps){
+    this.steps = steps
+  }
+  
+  public void startBuild(){
+    steps.echo "Hello, World"
+  }
+}
